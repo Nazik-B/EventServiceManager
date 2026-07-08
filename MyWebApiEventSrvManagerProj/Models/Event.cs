2c18759b1aa90bfaa484bc5132030c79e7408602
Nazik-B <nazik.bagdasaryan.93@mail.ru>
2026-07-08 22:30:11 +0300

    Complete manual API testing via Swagger UI

--- MyWebApiEventSrvManagerProj/Models/Event.cs
+++ MyWebApiEventSrvManagerProj/Models/Event.cs
@@ -1,14 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EventsApi.Models;
 
-public class Event
+public class Event : IValidatableObject
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Title is required")]
     public required string Title { get; set; }
 
     public string? Description { get; set; }
 
+    [Required(ErrorMessage = "StartAt is required")]
     public DateTime StartAt { get; set; }
 
+    [Required(ErrorMessage = "EndAt is required")]
     public DateTime EndAt { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndAt <= StartAt)
+        {
+            yield return new ValidationResult(
+                "EndAt must be later than StartAt",
+                new[] { nameof(EndAt) });
+        }
+    }
 }
--- MyWebApiEventSrvManagerProj/Program.cs
+++ MyWebApiEventSrvManagerProj/Program.cs
@@ -3,7 +3,8 @@ using EventsApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IEventService, EventService>();
 
@@ -11,10 +12,10 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
